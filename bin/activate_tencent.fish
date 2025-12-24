b79f1cc34c31732b591a441a9bac3d94632c81b2
Soloev's Uunu <alanqa-ops@outlook.com>
2025-12-24 12:48:25 +0800

    更新判断方式(未适配FLATPAK)

--- bin/activate_tencent.fish
+++ bin/activate_tencent.fish
@@ -12,34 +12,50 @@ set TRAYS ($EXPERIENCE_DIR/bin/get_notifiers)
 
 # Title,IconName,Id均为空的是QQ，Title=wechat & IconName='' & Id=wechat的是微信
 for tray in $TRAYS
-	if qdbus6 $tray 2>/dev/null | grep "/org/ayatana" 1>/dev/null
-		echo "$tray is a Ayatana indicator"
-		continue
-	end
-    set title (qdbus6 $tray /StatusNotifierItem org.freedesktop.DBus.Properties.Get org.kde.StatusNotifierItem Title)
+	# if qdbus6 $tray 2>/dev/null | grep "/org/ayatana" 1>/dev/null
+	# 	echo "$tray is a Ayatana indicator"
+	# 	continue
+	# end
+    # set title (qdbus6 $tray /StatusNotifierItem org.freedesktop.DBus.Properties.Get org.kde.StatusNotifierItem Title)
     # echo $title
-    set iconname (qdbus6 $tray /StatusNotifierItem org.freedesktop.DBus.Properties.Get org.kde.StatusNotifierItem IconName)
+    # set iconname (qdbus6 $tray /StatusNotifierItem org.freedesktop.DBus.Properties.Get org.kde.StatusNotifierItem IconName)
     # echo $iconname
-    set id (qdbus6 $tray /StatusNotifierItem org.freedesktop.DBus.Properties.Get org.kde.StatusNotifierItem Id)
+    # set id (qdbus6 $tray /StatusNotifierItem org.freedesktop.DBus.Properties.Get org.kde.StatusNotifierItem Id)
     # echo $id
+    set cmdline (cat /proc/(qdbus6 org.freedesktop.DBus /org/freedesktop/DBus org.freedesktop.DBus.GetConnectionUnixProcessID $tray)/cmdline)
 
     # 使用 switch 判断 ACTIVE_TYPE
     switch $ACTIVE_TYPE
         case QQ
+	    # 尝试直接通过cmdline识别
+	    if test "$cmdline" = "/opt/QQ/qq"
+                dbus-send --session --type=method_call --dest=$tray /StatusNotifierItem org.kde.StatusNotifierItem.Activate int32:0 int32:0
+        	exit 0
+            end
+	    	
             # 检测QQ：Title、IconName、Id 都为空
-            if test -z "$title" -a -z "$iconname" -a -z "$id"
-                echo "Try to activate QQ"
-                dbus-send --session --type=method_call --dest=$tray /StatusNotifierItem org.kde.StatusNotifierItem.Activate int32:0 int32:0
-                exit 0
-            end
-
+            # if qdbus6 $tray /MenuBar com.canonical.dbusmenu.Version
+            #     echo "Skip $tray: QQ should have no /MenuBar"
+            #     continue
+    	    # end
+            # if test -z "$title" -a -z "$iconname" -a -z "$id"
+            #     echo "Try to activate QQ $tray"
+            #     dbus-send --session --type=method_call --dest=$tray /StatusNotifierItem org.kde.StatusNotifierItem.Activate int32:0 int32:0
+            #     exit 0
+            # end
         case WeChat
-            # 检测微信：Title='wechat'，IconName为空，Id='wechat'
-            if test "$title" = "wechat" -a -z "$iconname" -a "$id" = "wechat"
-                echo "Try to ctivate Wecaht"
+            # 尝试直接通过cmdline识别
+            if test "$cmdline" = "/opt/wechat/wechat"
                 dbus-send --session --type=method_call --dest=$tray /StatusNotifierItem org.kde.StatusNotifierItem.Activate int32:0 int32:0
                 exit 0
             end
+            
+            # 检测微信：Title='wechat'，IconName为空，Id='wechat'
+            # if test "$title" = "wechat" -a -z "$iconname" -a "$id" = "wechat"
+            #     echo "Try to activate Wecaht"
+            #     dbus-send --session --type=method_call --dest=$tray /StatusNotifierItem org.kde.StatusNotifierItem.Activate int32:0 int32:0
+            #     exit 0
+            # end
 
         case '*'
             echo "Error: Unknown application type '$ACTIVE_TYPE'"
--- bin/launch_wechat.fish
+++ bin/launch_wechat.fish
@@ -1,15 +1,15 @@
 #!/bin/fish
 
-set has_running_wc (flatpak ps --columns=application | grep WeChat)
+set has_running_wc (ps aux | grep [/]opt/wechat/wechat)
 
 if ! test "$has_running_wc" = ""
     echo 'Request WeChat Wake'
     exec $EXPERIENCE_DIR/bin/activate_tencent.fish WeChat
 else
-    # 如果有参数传入，传递给 Flatpak
+    # 如果有参数传入，传递给 WC
     if set -q argv[1]
-        exec flatpak run --branch=stable --arch=x86_64 --command=wechat --file-forwarding com.tencent.WeChat $argv
+        exec /opt/wechat/wechat $argv
     else
-        exec flatpak run --branch=stable --arch=x86_64 --command=wechat com.tencent.WeChat
+        exec /opt/wechat/wechat
     end
 end
